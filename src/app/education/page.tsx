3398d25c002d5f342cf62a42b4aeb864113ea35e
MuhammadRukon <muhammad.rukon242@gmail.com>
2025-08-06 01:31:58 +0600

    feat: dummy pages added

---
+++ src/app/education/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import PageContainer from "@/components/page-container/page-container";
+import { usePageTitle } from "@/hooks/usePageTitle";
+
+export default function Achievements() {
+  const pageTitle = usePageTitle();
+
+  return <PageContainer page={pageTitle} />;
+}
